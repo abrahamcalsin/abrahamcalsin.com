37d43dc9fcf6b67fcaf21b4990d3ea2fcf47ac0b
abrahamcalsin <hello.abrahamcalsin@gmail.com>
2022-10-12 16:06:19 -0500

    refactor: format code

--- src/stories/Button.tsx
+++ src/stories/Button.tsx
@@ -1,41 +1,35 @@
-import React from 'react';
+import React from 'react'
 
-import './button.css';
+import './button.css'
 
 interface ButtonProps {
   /**
    * Is this the principal call to action on the page?
    */
-  primary?: boolean;
+  primary?: boolean
   /**
    * What background color to use
    */
-  backgroundColor?: string;
+  backgroundColor?: string
   /**
    * How large should the button be?
    */
-  size?: 'small' | 'medium' | 'large';
+  size?: 'small' | 'medium' | 'large'
   /**
    * Button contents
    */
-  label: string;
+  label: string
   /**
    * Optional click handler
    */
-  onClick?: () => void;
+  onClick?: () => void
 }
 
 /**
  * Primary UI component for user interaction
  */
-export const Button = ({
-  primary = false,
-  size = 'medium',
-  backgroundColor,
-  label,
-  ...props
-}: ButtonProps) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+export const Button = ({ primary = false, size = 'medium', backgroundColor, label, ...props }: ButtonProps) => {
+  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary'
   return (
     <button
       type="button"
@@ -45,5 +39,5 @@ export const Button = ({
     >
       {label}
     </button>
-  );
-};
+  )
+}
