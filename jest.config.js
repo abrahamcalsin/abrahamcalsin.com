29e09b09da63575aaaf33dca4f3cf307626c76f1
abrahamcalsin <hello.abrahamcalsin@gmail.com>
2022-10-12 22:28:57 -0500

    feat: add jest testing library configuration

---
+++ jest.config.js
@@ -0,0 +1,42 @@
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+/** @type {import('jest').Config} */
+const customJestConfig = {
+  displayName: '@abrahamcalsin/abrahamcalsin.com',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '~/*': '<rootDir/src/$1>',
+  },
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+  testEnvironment: 'jest-environment-jsdom',
+  coverageThreshold: {
+    global: {
+      statements: 80,
+      branches: 80,
+      functions: 80,
+      lines: 80,
+    },
+  },
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/**/index.{ts,tsx}',
+    '!src/typings/**/*.*',
+    '!src/themes/**/*.*',
+    '!src/mock-server/**/*.*',
+    '!src/services/**/*.*',
+    '!src/assets/**/*.*',
+    '!src/utils/constants/*.*',
+    '!src/**/*-styles.ts',
+    '!src/**/*.stories.tsx',
+    '!src/components/icons/**/*.tsx',
+  ],
+  watchPlugins: ['jest-watch-typeahead/filename', 'jest-watch-typeahead/testname'],
+}
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(customJestConfig)
---
+++ src/__tests__/basic.spec.ts
@@ -0,0 +1,8 @@
+/* eslint-disable no-undef */
+describe('Basic', () => {
+  it('sums two numbers', () => {
+    expect(5 + 5).toStrictEqual(10)
+  })
+})
+
+export {}
