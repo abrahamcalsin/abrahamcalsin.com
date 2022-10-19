5c656e40914467b15195b31604ebc2c5847a5b30
abrahamcalsin <hello.abrahamcalsin@gmail.com>
2022-10-18 22:05:34 -0500

    feat: add a new typing

---
+++ src/typings/components/button.ts
@@ -0,0 +1,25 @@
+import type * as React from 'react'
+import { ButtonProps as ChakraButtonProps } from '@chakra-ui/react'
+
+interface ButtonProps {
+  as?: ChakraButtonProps['as']
+  type?: ChakraButtonProps['type']
+  variant?: ChakraButtonProps['variant']
+  colorScheme?: ChakraButtonProps['colorScheme']
+  size?: ChakraButtonProps['size']
+  color?: string
+  href?: string
+  leftIcon?: ChakraButtonProps['leftIcon']
+  rightIcon?: ChakraButtonProps['rightIcon']
+  children?: ChakraButtonProps['children']
+  onClick?: () => void
+}
+
+type ButtonWrapperProps = Pick<
+  ButtonProps,
+  'as' | 'type' | 'variant' | 'colorScheme' | 'size' | 'color' | 'href' | 'leftIcon' | 'rightIcon' | 'onClick'
+> & {
+  children: React.ReactNode
+}
+
+export type { ButtonProps, ButtonWrapperProps }
