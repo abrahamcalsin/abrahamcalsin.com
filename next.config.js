32703a7a62ea2fcca4cbd6e7f4c36d0ab8705bbe
abrahamcalsin <hello.abrahamcalsin@gmail.com>
2022-11-06 18:24:56 -0500

    chore: add environment variable configuration

--- next.config.js
+++ next.config.js
@@ -3,4 +3,8 @@ const { withContentlayer } = require('next-contentlayer')
 /** @type {import('next').NextConfig} */
 module.exports = withContentlayer({
   reactStrictMode: true,
+  env: {
+    APP_ENV: process.env.APP_ENV,
+    API_ROUTE_SECRET: process.env.API_ROUTE_SECRET,
+  },
 })
---
+++ src/utils/env.ts
@@ -0,0 +1,5 @@
+const env = {
+  APP_ENV: process.env.APP_ENV || 'development',
+}
+
+export default env
