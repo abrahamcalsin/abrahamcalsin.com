8f555922eda5aabe2cba85c4c94c2973ad173872
abrahamcalsin <hello.abrahamcalsin@gmail.com>
2022-11-06 18:20:57 -0500

    feat: add an api route for projects

---
+++ pages/api/projects.ts
@@ -0,0 +1,25 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+import { notion } from '~/lib/notion/notion'
+import authMiddelware from '~/server/middlewares/auth-middleware'
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  await authMiddelware(req, res)
+
+  const response = await notion({
+    notionApiKey: process.env.NOTION_API_KEY!,
+    notionDatabaseId: process.env.NOTION_DATABASE_ID!,
+    sorts: [
+      {
+        property: 'startedAt',
+        direction: 'descending',
+      },
+    ],
+    filter: {
+      property: 'status',
+      select: { equals: 'published' },
+    },
+  })
+
+  res.status(200).json(response)
+}
